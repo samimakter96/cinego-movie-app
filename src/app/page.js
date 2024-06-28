952f16ae3fc9cd3fa0caa6b9766234527eb5cc9b
samim akter <samimakter9641@gmail.com>
2024-06-28 11:29:58 +0530

    add loading effect using next next js

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,8 @@ const Home = async ({ searchParams }) => {
   const response = await fetch(
     `https://api.themoviedb.org/3${
       genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
-    }?api_key=${API_KEY}&language=en-US&page=1`
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 10000 } }
   );
 
   if (!response.ok) {
@@ -26,9 +27,11 @@ const Home = async ({ searchParams }) => {
 
   const results = data.results;
 
-  return <div>
-    <Results results={results}/>
-  </div>;
+  return (
+    <div>
+      <Results results={results} />
+    </div>
+  );
 };
 
 export default Home;
