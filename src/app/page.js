0af9dbb6f7904cbe3928d59f3c13673348e185dd
samim akter <samimakter9641@gmail.com>
2024-06-28 10:53:40 +0530

    fetch data from tmdb api in the home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import Results from "@/components/Results";
 
-const page = () => {
-  return (
-    <div>Home</div>
-  )
-}
+const API_KEY = process.env.API_KEY;
 
-export default page
+const Home = async ({ searchParams }) => {
+  const genre = searchParams.genre || "fetchTrending";
+
+  const response = await fetch(
+    `https://api.themoviedb.org/3${
+      genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
+    }?api_key=${API_KEY}&language=en-US&page=1`
+  );
+
+  if (!response.ok) {
+    console.error("Failed to fetch data, status:", response.status);
+    throw new Error("Failed to fetch data");
+  }
+
+  let data;
+  try {
+    data = await response.json();
+  } catch (error) {
+    console.error("Error parsing JSON:", error);
+    throw new Error("Failed to parse JSON");
+  }
+
+  const results = data.results;
+
+  return <div>
+    <Results results={results}/>
+  </div>;
+};
+
+export default Home;
